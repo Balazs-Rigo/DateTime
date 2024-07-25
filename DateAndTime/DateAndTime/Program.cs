649262b92c36981f53ba50b248455c232c17df0c
BalazsRigo <rigo.balazs86@gmail.com>
2024-07-25 22:33:10 +0200

    commit 1

---
+++ DateAndTime/DateAndTime/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DateAndTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentLocalTime = DateTime.Now;
+
+            TimeZoneInfo sydneyTimeZone = TimeZoneInfo.FindSystemTimeZoneById("E. Australia Standard Time");
+
+            var currentLocalTimeInSydney = TimeZoneInfo.ConvertTime(currentLocalTime,sydneyTimeZone);
+
+            var currentDateTimeWithOffset = DateTimeOffset.Now;
+
+            var timeZones = TimeZoneInfo.GetSystemTimeZones();
+
+            Console.WriteLine($"Local time: { currentLocalTime}");
+            Console.WriteLine($"Sydney time: {currentLocalTimeInSydney}");
+            Console.WriteLine($"DateTimeOffset: {DateTimeOffset.Now}");
+        }
+    }
+}
