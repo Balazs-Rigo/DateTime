f31f7245e17447eb23a80d64759b3110d260d47c
balazsrigo <rigo.balazs86@gmail.com>
2024-07-27 18:36:27 +0200

    commit 4

--- DateAndTime/DateAndTime/Program.cs
+++ DateAndTime/DateAndTime/Program.cs
@@ -6,18 +6,15 @@ namespace DateAndTime
     {
         static void Main(string[] args)
         {
-            var input = "07/01/2024 10:00:00 PM";
-            var format = "M/d/yyyy hh:mm:ss tt";
+           var period = TimeSpan.FromSeconds(45);
 
-            var date = DateTimeOffset.ParseExact(input, format,
-                CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal);
+            var extendedPeriod = period.Multiply(2);
 
-            date = date.ToOffset(TimeSpan.FromHours(10));
+            var timeSpan = new TimeSpan(60, 100, 200);
 
-            var dateString = date.ToString("O");
+            Console.WriteLine(timeSpan.Hours);
 
-            Console.WriteLine($"input: {input}");
-            Console.WriteLine($"dateString: {dateString}");
+            Console.WriteLine($"utcnow offset: {DateTimeOffset.UtcNow}");
         }
     }
 }
