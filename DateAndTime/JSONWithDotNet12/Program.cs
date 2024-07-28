f097a616acc173ddcd1afe721174e2b0453c3f8c
balazsrigo <rigo.balazs86@gmail.com>
2024-07-28 23:22:09 +0200

    json commit 1

--- DateAndTime/JSONWithDotNet12/Program.cs
+++ DateAndTime/JSONWithDotNet12/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace JSONWithDotNet12
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace JSONWithDotNet12
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Author xavier = new Author()
+            {
+                Name = "Xavier Morera",
+                Courses = new string[] { "Solr","Spark","Jira","Python"}
+            };
+
+            Author lars = new Author()
+            {
+                Name = "lars klint",
+                Courses = new string[] { "Solr", "Spark", "Jira", "Python" }
+            };
+
+            Author jason = new Author()
+            {
+                Name = "jason alba",
+                Courses = new string[] { "Solr", "Spark", "Jira", "Python" }
+            };
+
+            xavier.FavoriteAuthors = new List<Author> { xavier, jason, lars, jason };
+
+            string xavierJson = JsonConvert.SerializeObject(xavier);
+
+            Console.WriteLine(xavierJson);
         }
     }
 }
